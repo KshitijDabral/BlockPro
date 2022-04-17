8d809293bcae34bc2be7480149c33d44c06c6aa8
unknown <kshitijdabral740@gmail.com>
2022-04-17 17:18:55 +0530

    added token.test.js

--- blockpro1/test/Token.test.js
+++ blockpro1/test/Token.test.js
@@ -1,5 +1,5 @@
 const Token = artifacts.require('./Token')
-require('chai').use(require('chai-as =-promised')).should()
+require('chai').use(require('chai-as-promised')).should()
 
 contract('Token',(accounts)=>{
     describe('deployment',()=>{
